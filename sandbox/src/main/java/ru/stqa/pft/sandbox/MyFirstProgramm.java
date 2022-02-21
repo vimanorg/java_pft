7667ee84315a74746fd2a0c85aa43d7fd0680866
vimanorg <vimanorg88@gmail.com>
2022-02-22 03:45:23 +0500

    Проект отвязан от репозитория Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgramm {
-public static void main (String[] args) {
-System.out.println("Hello, world!");
- }
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
