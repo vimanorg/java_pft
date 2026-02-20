41b5db14bcd56931f092194db4c860e9357f830e
vimanorg <100158316+vimanorg@users.noreply.github.com>
2026-02-20 18:24:39 +0500

    Добавлен Square Tests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,8 +11,6 @@ public class MyFirstProgram {
 
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
     Rectangle r = new Rectangle(4, 6);
-    r.a = 4;
-    r.b = 6;
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     // Тестовые точки
 
