6039cff530ee4336c09d134a6cd4838480ca83a3
vimanorg <100158316+vimanorg@users.noreply.github.com>
2026-03-03 20:18:42 +0500

    Добавлены тесты для Equation содержащие свернуnые конструкции if

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -12,25 +12,30 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d =b*b - 4*a*c;
-
-    if (d > 0) {
-      n=2;
-    } else {
-      if (d==0) {
+    double d = b * b - 4 * a * c;
+    if (a != 0)
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
-      }
-      else {
+      } else {
         n = 0;
       }
+    else if (b != 0) {
+
+      n = 1;
+    }
+      else if (c == 0) {
+        n = -1;
+      } else {
+        n = 0;
+      }
+    }
+
+    public int rootNumber () {
+      return n;
 
     }
   }
-
-  public int rootNumber() {
-    return n;
-
-  }
-}
 
 
