93d2a841ffcace18337e6bebeb7a8b35bc1864cb
vimanorg <100158316+vimanorg@users.noreply.github.com>
2026-03-03 16:29:38 +0500

    Завершение рефакторинга кода и дробления appmanager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,12 +1,8 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class ApplicationManager {
@@ -20,8 +16,6 @@ public class ApplicationManager {
     options.setBinary("C:\\Users\\fayanovav\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
     wd = new FirefoxDriver(options);
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-
     navigationHelper = new NavigationHelper(wd);
     groupHelper = new GroupHelper(wd);
     navigationHelper.goToGrupePage(wd);
@@ -35,29 +29,9 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
   public GroupHelper getGroupHelper() {
     return groupHelper;
   }
 
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
-  }
+
 }
