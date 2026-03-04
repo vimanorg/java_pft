3a034ab5897a8269b6891ee05399a4ffc0f902e5
vimanorg <100158316+vimanorg@users.noreply.github.com>
2026-03-04 15:24:21 +0500

    Тесты проходят успешно убраны  лишние комментарии в коде

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/WorkGroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/WorkGroupCreationTests.java
@@ -3,25 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-/**
- * Тест-кейс для проверки создания группы в приложении Address Book.
- * Выполняет полный сценарий: вход в систему → создание группы
- * Использует Selenium WebDriver для автоматизации взаимодействия с веб-интерфейсом.
- */
+
 public class WorkGroupCreationTests extends TestBase {
 
-  /**
-   * Основной тест: создание новой группы.
-   * Шаги:
-   * 1. Открытие страницы входа.
-   * 2. Ввод логина и пароля.
-   * 3. Авторизация.
-   * 4. Переход к созданию группы.
-   * 5. Заполнение полей: имя, заголовок, подвал группы.
-   * 6. Сохранение группы.
-   * 7. Переход к списку групп.
-   * 8. Выход из системы.
-   */
   @Test
   public void testGroupCreation() throws Exception {
     app.getGroupHelper().returnToGroupPage();// Переход к созданию группы
