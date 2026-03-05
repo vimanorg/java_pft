8bf9c44543beecf8aece33ba658fa0abdf5643bd
vimanorg <vimanorg88@gmail.com>
2026-03-05 10:35:43 +0500

    добавил локатор для home

--- addressbook-web-tests/src/test/java/home/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/home/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -21,15 +21,19 @@ public class GroupHelper {
     }
 
     public void fillGroupForm(GroupData groupData) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).clear();
-      wd.findElement(By.name("group_name")).sendKeys(groupData.name());
-      wd.findElement(By.name("group_header")).click();
+        type(groupData, By.name("group_name"));
+        wd.findElement(By.name("group_header")).click();
       wd.findElement(By.name("group_header")).clear();
       wd.findElement(By.name("group_header")).sendKeys(groupData.header());
       wd.findElement(By.name("group_footer")).click();
       wd.findElement(By.name("group_footer")).clear();
       wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+    }
+
+    private void type(GroupData groupData, By locator) {
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(groupData.name());
     }
 
     public void initGroupCreation() {
