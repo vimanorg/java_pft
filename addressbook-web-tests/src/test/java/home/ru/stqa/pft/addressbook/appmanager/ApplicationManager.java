6af93a072795cb07e45c2f89e5b82194c7ead438
vimanorg <vimanorg88@gmail.com>
2026-03-09 16:18:44 +0500

    Промежуточное сохранение для home. Добавлено условие для нескольких браузеров

--- addressbook-web-tests/src/test/java/home/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/home/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,9 @@
 package home.ru.stqa.pft.addressbook.appmanager;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import java.time.Duration;
 
 public class ApplicationManager {
@@ -13,6 +14,14 @@ public class ApplicationManager {
     private  GroupHelper groupHelper;
 
     public void init() {
+        Browser BrowserType = null;
+        Browser browser = BrowserType.FIREFOX;
+        if (browser == BrowserType.FIREFOX) {wd = new FirefoxDriver();
+        }
+        else if (browser ==BrowserType.CHROME) { wd = new ChromeDriver();
+        }
+        else if (browser == BrowserType.IE) {wd = new internetExplorerDriver();
+        }
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         wd.get("http://localhost/addressbook/group.php");
---
+++ addressbook-web-tests/src/test/java/home/ru/stqa/pft/addressbook/appmanager/internetExplorerDriver.java
@@ -0,0 +1,76 @@
+package home.ru.stqa.pft.addressbook.appmanager;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+public class internetExplorerDriver implements WebDriver {
+    @Override
+    public void get(String url) {
+
+    }
+
+    @Override
+    public @Nullable String getCurrentUrl() {
+        return "";
+    }
+
+    @Override
+    public @Nullable String getTitle() {
+        return "";
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        return List.of();
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return null;
+    }
+
+    @Override
+    public @Nullable String getPageSource() {
+        return "";
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return Set.of();
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return "";
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return null;
+    }
+
+    @Override
+    public Navigation navigate() {
+        return null;
+    }
+
+    @Override
+    public Options manage() {
+        return null;
+    }
+}
